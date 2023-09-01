3d62514cbe8b5c331925399f2560f45cc336ebc7
Goldjay94 <genevieveezeh786@gmail.com>
2023-09-01 23:44:54 +0000

    updated tasks

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+import router from './routes';
 
-const server = express();
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use('/', router);
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
-
-export default server;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
